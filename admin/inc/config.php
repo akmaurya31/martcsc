1d3b2ac426990d44425d70a38029b16a0ff68bf7
aqadavinash <avinash@aqad.ae>
2024-09-25 21:36:12 +0530

    ad

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -9,7 +9,7 @@ date_default_timezone_set('America/Los_Angeles');
 $dbhost = 'localhost';
 
 // Database Name
-$dbname = 'ecommerceweb';
+$dbname = 'ecommerceweb1';
 
 // Database Username
 $dbuser = 'root';
